f38e4bfc11f7b9a6f59450eea59000f53e79cd26
Mitchell Borchers <30349201+borchr27@users.noreply.github.com>
2021-01-31 21:29:16 -0500

    fixing navbar, adding more pages

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template  # importing the render_template functi
 
 app = Flask(__name__)
 # home route
-@app.route("/")
-def hello():
-    return render_template('index.html')
+@app.route("/home")
+def Home():
+    return render_template('home.html')
+
+@app.route("/contact")
+def Contact():
+    return render_template('contact.html')
+
+@app.route("/about")
+def About():
+    return render_template('about.html')
 
 app.run(debug = True) 
